708fc4d0927d7f9b2dee7b8c79277b30d210d767
Christina Reissel <christina.reissel@cern.ch>
2025-09-15 10:20:14 -0400

    setup PyTorchLightning pipeline #2: S4D module

---
+++ model.py
@@ -0,0 +1,65 @@
+from models.s4d import S4D
+from models.networks import S4DModel
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.optim as optim
+import lightning as L
+import numpy as np
+from io import BytesIO
+import matplotlib.pyplot as plt
+
+class LitModel(L.LightningModule):
+    def __init__(self, d_input, d_output, encoder: nn.Module, loss='MSELoss'):
+        super().__init__()
+
+        self.encoder = encoder
+        self.loss = loss
+        self.d_output = d_output
+        if self.loss=='MSELoss':
+            self.criterion = nn.MSELoss()
+        self.save_hyperparameters()
+
+    def __loss__(self, X, y):
+        y_preds = self.forward(X)
+        if self.loss=='MSELoss':
+            return self.criterion(y, y_preds)
+
+    def forward(self, x):
+        x = self.encoder(x)  
+        return x
+
+    def configure_optimizers(self):
+        optimizer = optim.AdamW(self.parameters(), lr=0.01)
+        scheduler = optim.lr_scheduler.ExponentialLR(optimizer, gamma=0.99)
+        return optimizer, scheduler
+
+    def training_step(self, batch, batch_idx, log=True):
+        X, y, _ = batch
+        loss = self.__loss__(X, y)
+
+        if log:
+            self.log("train/loss",
+                    loss,
+                    on_step=False,
+                    on_epoch=True,
+                    reduce_fx='mean',
+                    logger=True,
+                    prog_bar=True)
+
+        return loss
+
+    def validation_step(self, batch, batch_idx, log=True):
+        X, y, _ = batch
+        loss = self.__loss__(X, y)
+
+        if log:
+            self.log("val/loss",
+                    loss,
+                    on_step=False,
+                    on_epoch=True,
+                    reduce_fx='mean',
+                    logger=True,
+                    prog_bar=True)
+
+        return loss
